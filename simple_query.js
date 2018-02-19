f54e919f626e4f36faaf8df9b204316d7901c878
unknown <fatimah.azzhr@gmail.com>
2018-02-19 19:17:50 +0700

    poll db2 done

--- simple_query.js
+++ simple_query.js
@@ -5,27 +5,26 @@ const db = new sqlite3.Database("./poll.db", sqlite3.OPEN_READWRITE, err => {
 });
 
 db.serialize(() => {
-  db.all(`SELECT name, party, grade_current FROM Politicians WHERE party = 'R' AND grade_current BETWEEN 9 AND 11`, (err, rows) => {
+  db.all(`SELECT name, location, grade_current, COUNT(*) AS totalVote 
+         FROM Votes LEFT JOIN Politicians ON Politicians.id = Votes.politicianId 
+         WHERE grade_current < 9 GROUP BY name ORDER BY grade_current`, (err, rows) => {
     if(err) throw err
     console.log(rows)
   })
 
-  db.all(`SELECT COUNT(*) AS totalVote, Politicians.name FROM Votes LEFT JOIN Politicians ON Politicians.id = Votes.politicianId WHERE Politicians.name = 'Olympia Snowe'`, (err, rows) => {
+  db.all(`SELECT (SELECT COUNT(*) FROM Votes WHERE Votes.politicianId = Politicians.id) AS totalVote, name AS politicianName, 
+         first_name || ' ' || last_name AS voterName, gender FROM Votes 
+         LEFT JOIN Politicians ON Politicians.id = Votes.politicianId 
+         LEFT JOIN Voters ON Voters.id = Votes.voterId 
+         WHERE Politicians.name IN ('Candice Miller', 'Adam Smith', 'Anna Eshoo') 
+         ORDER BY totalVote DESC`, (err, rows) => {
     if(err) throw err
     console.log(rows)
   })
 
-  db.all(`SELECT Politicians.name, COUNT(*) AS totalVote FROM Votes LEFT JOIN Politicians ON Politicians.id = Votes.politicianId WHERE Politicians.name LIKE 'Adam%' GROUP BY Politicians.name`, (err, rows) => {
-    if(err) throw err
-    console.log(rows)
-  })
-
-  db.all(`SELECT COUNT(*) AS totalVote, Politicians.name, Politicians.party, Politicians.location FROM Votes LEFT JOIN Politicians ON Politicians.id = Votes.politicianId GROUP BY Politicians.name ORDER BY totalVote DESC LIMIT 3`, (err, rows) => {
-    if(err) throw err
-    console.log(rows)
-  })
-
-  db.all(`SELECT Voters.first_name, Voters.last_name, Voters.gender, Voters.age FROM Votes LEFT JOIN Politicians ON Politicians.id = Votes.politicianId LEFT JOIN Voters ON Voters.id = Votes.voterId WHERE Politicians.name = 'Olympia Snowe'`, (err, rows) => {
+  db.all(`SELECT COUNT(*) AS totalVote, first_name || ' ' || last_name AS name, gender, age FROM Votes 
+         LEFT JOIN Voters ON Voters.id = Votes.voterId 
+         GROUP BY voterId HAVING totalVote > 1 ORDER BY totalVote DESC`, (err, rows) => {
     if(err) throw err
     console.log(rows)
   })
