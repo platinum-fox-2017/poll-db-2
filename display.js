e5acd273f7b92876f9b7a3c1acc30dd1132ed623
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-19 17:15:31 +0700

    Release 0: Advanced queries to expose voter fraud

--- display.js
+++ display.js
@@ -1,36 +1,47 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('poll');
 db.serialize(() => {
-  let query = "SELECT * FROM Politicians WHERE party = 'R' AND grade_current >= 9 AND grade_current <= 11";
-  db.each(query,(err,row) =>{
-    console.log(`Name: ${row.name} , Party: ${row.party}, Grade Current: ${row.grade_current}`);
-  });
-  query = "SELECT COUNT(*) AS totalVotes , Politicians.name AS name FROM Votes ";
+  //berapa banyak vote yang diterima Politicians yang memiliki grade di bawah 9
+  query = "SELECT COUNT(*) AS totalVotes , Politicians.* FROM Votes ";
   query += "LEFT JOIN Politicians ON Politicians.id = Votes.politicianId ";
-  query += "WHERE Politicians.name = 'Olympia Snowe'";
+  query += "WHERE Politicians.grade_current < 9 "
+  query += "GROUP BY Politicians.name ORDER BY totalVotes ASC ";
   db.each(query,(err,row) =>{
-    console.log(`Total Votes: ${row.totalVotes} , Name: ${row.name}`);
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Name: ${row.name}, Location: ${row.location}, Grade Current: ${row.grade_current}, Total Vote: ${row.totalVotes}`);
+    }
   });
-  console.log('Yang Namanya Ada Adamnya');
-  query = "SELECT COUNT(*) AS totalVotes , Politicians.name AS name FROM Votes ";
+  //3 Politicians yang memiliki vote terbanyak dan siapa saja yang memilih politician tersebut
+  // console.log('Politicians yang grade nya di bawah 9')
+  query = "SELECT  Voters.*, Politicians.*,( "
+  query += "SELECT COUNT(*) FROM Votes"
+  query += " WHERE Politicians.id = Votes.politicianId  ";
+  query += ") AS totalVotes  FROM Votes  "
+  query += "LEFT JOIN Voters ON Voters.id = Votes.voterId "
+  query += "LEFT JOIN Politicians ON Politicians.id = Votes.politicianId "
+  query += "WHERE Votes.politicianId IN ("
+  query += "SELECT  Politicians.id FROM Votes ";
   query += "LEFT JOIN Politicians ON Politicians.id = Votes.politicianId ";
-  query += "WHERE Politicians.name LIKE '%Adam%' GROUP BY Politicians.name";
+  query += "GROUP BY Politicians.name ORDER BY COUNT(*) DESC  LIMIT 3 )";
+  query += "ORDER BY politicianId"
   db.each(query,(err,row) =>{
-    console.log(`Total Votes: ${row.totalVotes} , Name: ${row.name}`);
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Full Name: ${row.first_name} ${row.last_name}, Voter Gender: ${row.gender} Politician Name: ${row.name}, Total Vote: ${row.totalVotes}`);
+    }
   });
-  console.log('3 Suara Terbanyak');
-  query = "SELECT COUNT(*) AS totalVotes , Politicians.name AS name FROM Votes ";
-  query += "LEFT JOIN Politicians ON Politicians.id = Votes.politicianId ";
-  query += "GROUP BY Politicians.name ORDER BY totalVotes DESC LIMIT 3";
+  //tampilkan voter yang melakukan vote lbih dari satu kali,
+  query = "SELECT COUNT(*) AS totalVotes, Voters.* FROM Votes  "
+  query += "LEFT JOIN Voters ON Voters.id = Votes.voterId "
+  query += "GROUP BY voterId  HAVING totalVotes > 1"
   db.each(query,(err,row) =>{
-    console.log(`Total Votes: ${row.totalVotes} , Name: ${row.name}`);
-  });
-  console.log('Yang Milih Olympia Snowe');
-  query = "SELECT Voters.* FROM Votes ";
-  query += "LEFT JOIN Politicians ON Politicians.id = Votes.politicianId ";
-  query += "LEFT JOIN Voters ON Voters.id = Votes.voterId ";
-  query += "WHERE Politicians.name = 'Olympia Snowe'";
-  db.each(query,(err,row) =>{
-    console.log(`First Name : ${row.first_name}, Last Name : ${row.last_name}, Gender : ${row.gender}, Age: ${row.age}`);
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Full Name: ${row.first_name} ${row.last_name}, Voter Gender: ${row.gender} , Voter Age: ${row.age} , Total Vote: ${row.totalVotes}`);
+    }
   });
 })
